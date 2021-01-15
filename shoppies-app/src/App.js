781e10b1ba7e7b33b9be579691cc71ad32cc76c4
Scorpio <diallomikey13@yahoo.fr>
2021-01-15 01:19:55 -0500

    update

--- shoppies-app/src/App.js
+++ shoppies-app/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Components/Header.js';
+import './tailwind.css';
+import SearchMovies from './Components/Search.js';
+import Results from './Components/Results';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="relative width-full">
+        <div className="mx-auto overflow-hidden">
+          <Header/>
+          <SearchMovies/>
+        </div>
+      </div>
   );
 }
 
